91e83ac708392e832a2917632e87f8d9f55ed7b2
pwheregroup <25791018+pwheregroup@users.noreply.github.com>
2020-02-17 10:24:44 +0100

    Add PSR12 ruleset

--- src/RuleSet.php
+++ src/RuleSet.php
@@ -14,6 +14,8 @@ namespace PhpCsFixer;
 
 use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitTargetVersion;
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use PhpCsFixer\Fixer\Basic\BracesFixer;
 
 /**
  * Set of rules to be used by fixer.
@@ -55,6 +57,51 @@ final class RuleSet implements RuleSetInterface
             'switch_case_semicolon_to_colon' => true,
             'switch_case_space' => true,
             'visibility_required' => true,
+        ],
+        '@PSR12' => [
+            '@PSR2' => true,
+            'binary_operator_spaces' => true,
+            'blank_line_after_opening_tag' => true,
+            'braces' => [
+                'allow_single_line_closure' => false,
+                'position_after_anonymous_constructs' => BracesFixer::LINE_SAME,
+                'position_after_control_structures' => BracesFixer::LINE_SAME,
+                'position_after_functions_and_oop_constructs' => BracesFixer::LINE_NEXT,
+            ],
+            'concat_space' => ['spacing' => 'one'],
+            'declare_equal_normalize' => [
+                'space' => 'none'
+            ],
+            'lowercase_cast' => true,
+            'new_with_braces' => true,
+            'no_blank_lines_after_class_opening' => true,
+            'no_extra_blank_lines' => false,
+            'no_leading_import_slash' => true,
+            'no_singleline_whitespace_before_semicolons' => true,
+            'no_trailing_whitespace' => true,
+            'no_whitespace_before_comma_in_array' => true,
+            'ordered_class_elements' => [
+                'order' => ['use_trait']
+            ],
+            'ordered_imports' => [
+                'importsOrder' => [
+                    OrderedImportsFixer::IMPORT_TYPE_CLASS,
+                    OrderedImportsFixer::IMPORT_TYPE_CONST,
+                    OrderedImportsFixer::IMPORT_TYPE_FUNCTION,
+                ]
+            ],
+            'return_type_declaration' => true,
+            'short_scalar_cast' => true,
+            'single_import_per_statement' => false,
+            'space_after_semicolon' => [
+                'remove_in_empty_for_expressions' => true,
+            ],
+            'ternary_operator_spaces' => true,
+            'unary_operator_spaces' => true,
+            'visibility_required' => [
+                'elements' => ['const', 'method', 'property']
+            ],
+            'whitespace_after_comma_in_array' => true,
         ],
         '@Symfony' => [
             '@PSR2' => true,
